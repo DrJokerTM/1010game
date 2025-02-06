5bb1bb7da060832f73d8f6fb1f5cfc1cb4cce0e3
David Sochor <davidsochor@seznam.cz>
2025-02-07 00:57:55 +0100

    Uklid kodu

--- storage.js
+++
@@ -1,9 +0,0 @@
-// Funkce pro práci s IndexedDB nebo localStorage
-
-export function saveGameState(gameState) {
-    localStorage.setItem("currentGame", JSON.stringify(gameState));
-}
-
-export function loadGameState() {
-    return JSON.parse(localStorage.getItem("currentGame"));
-}
